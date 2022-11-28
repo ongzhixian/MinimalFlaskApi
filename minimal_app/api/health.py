66df61aec7ee760c27b7676899f6cfeec3257263
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-11-28 10:40:22 +0800

    update

--- minimal_app/api/health.py
+++ minimal_app/api/health.py
@@ -3,7 +3,7 @@
 # Modules and functions import statements
 ################################################################################
 
-import logging
+from minimal_app.features.logging import log
 from flask import request, make_response, abort
 from minimal_app import app, app_secrets
 
@@ -11,8 +11,8 @@ from minimal_app import app, app_secrets
 def api_health():
     try:
         response_message = "OK"
-        logging.info("response_message")
+        log.info("response_message")
         return response_message
     except Exception as e:
-        logging.info("ERROR----------ERROR----------")
-        logging.error(e)
+        log.info("ERROR----------ERROR----------")
+        log.error(e)
